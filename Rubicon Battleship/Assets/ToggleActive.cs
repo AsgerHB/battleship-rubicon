d35f21d170884fa55f28f07586e85327b235bee3
ThLyn <thomas.l.madsen.tm@gmail.com>
2019-08-24 19:37:25 +0200

    TA

---
+++ Rubicon Battleship/Assets/ToggleActive.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleActive : MonoBehaviour
+
+{
+    public void foo()
+    {
+        Debug.Log("start");
+        if (Active == true)
+        {
+            Active = false;
+        }
+        else
+        {
+            Active = true;
+        }
+        
+        
+    }
+
+    public GameObject ArrowN;
+    public GameObject ArrowS;
+    public GameObject ArrowE;
+    public GameObject ArrowW;
+
+    public bool Active;
+
+    void Update()
+    {
+        ArrowN.SetActive(Active);
+        ArrowS.SetActive(Active);
+        ArrowE.SetActive(Active);
+        ArrowW.SetActive(Active);
+    }
+}
