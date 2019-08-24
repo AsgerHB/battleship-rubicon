ecf4100cb35c6627e650e9e644b5eaa004a91d9e
ThLyn <thomas.l.madsen.tm@gmail.com>
2019-08-24 16:33:35 +0200

    Click file

---
+++ Rubicon Battleship/Assets/Click.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Click : MonoBehaviour
+
+{
+    public UnityEvent Clicked;
+    public Collider coll;
+
+    void Start()
+    {
+        if (Clicked == null)
+            Clicked = new UnityEvent();
+
+        Clicked.AddListener(Pawn);
+        coll = GetComponent<Collider>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && Clicked != null)
+        {
+           Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (coll.Raycast(ray, out hit, 100.0f))
+            {
+                Clicked.Invoke();
+            } 
+        }
+    }
+
+    void Pawn()
+    {
+        Debug.Log("ClickSelect");
+    }
+}
