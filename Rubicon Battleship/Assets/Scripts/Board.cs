204323b8d52b8cc608a264b1808aae6dd54aaa79
Asger Horn Brorholt <ab@carbuddii.com>
2019-08-24 15:13:38 +0200

    Board and Board pieces

---
+++ Rubicon Battleship/Assets/Scripts/Board.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int Rows;
+    public int Columns;
+    public int Width;
+    public int Height;
+}
