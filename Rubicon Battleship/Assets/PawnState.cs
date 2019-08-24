c87c8b7085a453f05c10e5a849d5de1ebe1b2be1
Asger Horn Brorholt <ab@carbuddii.com>
2019-08-24 16:17:20 +0200

    MoveQueue

--- Rubicon Battleship/Assets/PawnState.cs
+++ Rubicon Battleship/Assets/PawnState.cs
@@ -8,6 +8,14 @@ public class PawnState : MonoBehaviour
     public int health;
     public bool veteran;
     
+    public int GetMaxMoves()
+    {
+        if (veteran)
+            return 5;
+        else
+            return 4;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
---
+++ Rubicon Battleship/Assets/Scripts/MoveQueque.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public enum Direction { Up, Down, Left, Right}
+
+[System.Serializable]
+public class Move {
+    public Direction Direction;
+    public int Count;
+}
+
+public class MoveQueque : MonoBehaviour
+{
+    public List<Move> Queque = new List<Move>();
+    public PawnState State;
+
+    public void AddMove(Direction direction){
+        Debug.Log(Queque.Count());
+        if (MoveCountSum() == State.GetMaxMoves())
+        {
+            Debug.Log("Move count exceeded");
+            return;
+        }
+
+        State = transform.GetComponent<PawnState>();
+
+        if(Queque.Count() != 0 && Queque.Last().Direction == direction){
+            Queque.Last().Count++;
+        }
+        else
+        {
+            Queque.Add(new Move { Count = 1, Direction = direction});
+        }
+
+    }
+
+    public int MoveCountSum()
+    {
+        var count = 0;
+        foreach (var move in Queque)
+        {
+            count += move.Count;
+        }
+
+        return count;
+    }
+
+    [ContextMenu("Up")]
+    public void Up(){
+        AddMove(Direction.Up);
+    }
+
+    
+    [ContextMenu("Down")]
+    public void Down(){
+        AddMove(Direction.Down);
+    }
+
+    
+    [ContextMenu("Left")]
+    public void Left(){
+        AddMove(Direction.Left);
+    }
+    
+    
+    [ContextMenu("Right")]
+    public void Right(){
+        AddMove(Direction.Right);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
