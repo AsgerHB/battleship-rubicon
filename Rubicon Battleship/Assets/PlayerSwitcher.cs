8bee51576a505a02f702a5373068c33b5c9e09b8
Asger Horn Brorholt <ab@carbuddii.com>
2019-08-25 14:00:59 +0200

    PlayerSwitcher

---
+++ Rubicon Battleship/Assets/PlayerSwitcher.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSwitcher : MonoBehaviour
+{
+    public GameObject Camera;
+    public GameObject Player1;
+    public GameObject Player2;
+
+    int CurrentPlayer = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+            CurrentPlayer = 1;
+            Camera.transform.position = Player1.transform.position;
+            Camera.transform.rotation = Player1.transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    [ContextMenu("Switch")]
+    public void Switch(){
+        if(CurrentPlayer == 1){
+            CurrentPlayer = 2;
+            Camera.transform.position = Player2.transform.position;
+            Camera.transform.rotation = Player2.transform.rotation;
+        }
+        else
+        {
+            CurrentPlayer = 1;
+            Camera.transform.position = Player1.transform.position;
+            Camera.transform.rotation = Player1.transform.rotation;
+        }
+    }
+}
